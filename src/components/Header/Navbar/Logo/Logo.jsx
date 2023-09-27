0392a6127eb1c13a7b0a972acb333d92b3a53ec6
assadujjaman112 <assadujjamanmurad120@gmail.com>
2023-09-27 15:14:20 +0600

    completed the project

--- src/components/Header/Navbar/Logo/Logo.jsx
+++ src/components/Header/Navbar/Logo/Logo.jsx
@@ -2,7 +2,7 @@
 const Logo = () => {
     return (
         <div>
-            <img src="/public/assets/Logo.png" alt="" />
+            <img src="https://i.ibb.co/4t9pm5S/Logo.png" alt="Logo" border="0"/>
             
         </div>
     );
